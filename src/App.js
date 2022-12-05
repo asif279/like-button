08d48715ec257c7c5793cb1ec311923477579844
asif279 <asifofficial8130@gmail.com>
2022-12-06 03:05:33 +0600

    ss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import ThumbUpRoundedIcon from '@mui/icons-material/ThumbUpRounded';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [like,setLike]=useState('');
+   const handleLike=()=>{
+    
+   
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <ThumbUpRoundedIcon onClick={()=>setLike(like ? '':"primary")} color={like}></ThumbUpRoundedIcon>
+      
     </div>
   );
 }
